c5f069d4d0046cba0b609dd878b595b668989aa6
Fran Pruna <fprunamorejon@gmail.com>
2025-02-10 15:29:14 +0100

    creadas rutas y controlador de Login

--- backend/app/Models/Usuario.php
+++ backend/app/Models/Usuario.php
@@ -23,7 +23,7 @@ class Usuario extends Model
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
-     */ 
+     */
     protected $fillable = [
         'nombre',
         'NIF',
